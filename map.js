6db38ea886129ab53366b026302a08b3a8ed5ede
Masao Yano <masao.yano@gmail.com>
2025-03-01 15:10:10 +0900

    マップ表示

---
+++ map.js
@@ -0,0 +1,34 @@
+// MapLibre GL JSの読み込み
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+const map = new maplibregl.Map({
+    container: 'map', // div要素のid
+    zoom: 5, // 初期表示のズーム
+    center: [138, 37], // 初期表示の中心
+    minZoom: 5, // 最小ズーム
+    maxZoom: 18, // 最大ズーム
+    maxBounds: [122, 20, 154, 50], // 表示可能な範囲
+    style: {
+        version: 8,
+        sources: {
+            // 背景地図ソース
+            osm: {
+                type: 'raster',
+                tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
+                maxzoom: 19,
+                tileSize: 256,
+                attribution:
+                    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            },
+        },
+        layers: [
+            // 背景地図レイヤー
+            {
+                id: 'osm-layer',
+                source: 'osm',
+                type: 'raster',
+            },
+        ],
+    },
+});
