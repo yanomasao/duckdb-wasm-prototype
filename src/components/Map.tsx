9332597b33ca440c63712e5f69d9d5696d9acb7a
Masao Yano <masao.yano@gmail.com>
2025-04-01 11:48:32 +0000

    ハザードマップを削除

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,5 @@
 import * as duckdb from "@duckdb/duckdb-wasm";
 import maplibregl from "maplibre-gl";
-import OpacityControl from "maplibre-gl-opacity";
 import "maplibre-gl-opacity/dist/maplibre-gl-opacity.css";
 import "maplibre-gl/dist/maplibre-gl.css";
 import React, { useEffect, useState } from "react";
@@ -60,72 +59,6 @@ const Map: React.FC<MapProps> = ({ points = [], db, selectedColumns }) => {
                         attribution:
                             '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
                     },
-                    hazard_flood: {
-                        type: "raster",
-                        tiles: [
-                            "https://disaportaldata.gsi.go.jp/raster/01_flood_l2_shinsuishin_data/{z}/{x}/{y}.png",
-                        ],
-                        minzoom: 2,
-                        maxzoom: 17,
-                        tileSize: 256,
-                        attribution:
-                            '<a href="https://disaportal.gsi.go.jp/hazardmap/copyright/opendata.html">ハザードマップポータルサイト</a>',
-                    },
-                    hazard_hightide: {
-                        type: "raster",
-                        tiles: [
-                            "https://disaportaldata.gsi.go.jp/raster/03_hightide_l2_shinsuishin_data/{z}/{x}/{y}.png",
-                        ],
-                        minzoom: 2,
-                        maxzoom: 17,
-                        tileSize: 256,
-                        attribution:
-                            '<a href="https://disaportal.gsi.go.jp/hazardmap/copyright/opendata.html">ハザードマップポータルサイト</a>',
-                    },
-                    hazard_tsunami: {
-                        type: "raster",
-                        tiles: [
-                            "https://disaportaldata.gsi.go.jp/raster/04_tsunami_newlegend_data/{z}/{x}/{y}.png",
-                        ],
-                        minzoom: 2,
-                        maxzoom: 17,
-                        tileSize: 256,
-                        attribution:
-                            '<a href="https://disaportal.gsi.go.jp/hazardmap/copyright/opendata.html">ハザードマップポータルサイト</a>',
-                    },
-                    hazard_doseki: {
-                        type: "raster",
-                        tiles: [
-                            "https://disaportaldata.gsi.go.jp/raster/05_dosekiryukeikaikuiki/{z}/{x}/{y}.png",
-                        ],
-                        minzoom: 2,
-                        maxzoom: 17,
-                        tileSize: 256,
-                        attribution:
-                            '<a href="https://disaportal.gsi.go.jp/hazardmap/copyright/opendata.html">ハザードマップポータルサイト</a>',
-                    },
-                    hazard_kyukeisha: {
-                        type: "raster",
-                        tiles: [
-                            "https://disaportaldata.gsi.go.jp/raster/05_kyukeishakeikaikuiki/{z}/{x}/{y}.png",
-                        ],
-                        minzoom: 2,
-                        maxzoom: 17,
-                        tileSize: 256,
-                        attribution:
-                            '<a href="https://disaportal.gsi.go.jp/hazardmap/copyright/opendata.html">ハザードマップポータルサイト</a>',
-                    },
-                    hazard_jisuberi: {
-                        type: "raster",
-                        tiles: [
-                            "https://disaportaldata.gsi.go.jp/raster/05_jisuberikeikaikuiki/{z}/{x}/{y}.png",
-                        ],
-                        minzoom: 2,
-                        maxzoom: 17,
-                        tileSize: 256,
-                        attribution:
-                            '<a href="https://disaportal.gsi.go.jp/hazardmap/copyright/opendata.html">ハザードマップポータルサイト</a>',
-                    },
                 },
                 layers: [
                     {
@@ -140,66 +73,11 @@ const Map: React.FC<MapProps> = ({ points = [], db, selectedColumns }) => {
                         paint: { "raster-opacity": 0 },
                         layout: { visibility: "visible" },
                     },
-                    {
-                        id: "hazard_flood-layer",
-                        source: "hazard_flood",
-                        type: "raster",
-                        paint: { "raster-opacity": 0.7 },
-                        layout: { visibility: "none" },
-                    },
-                    {
-                        id: "hazard_hightide-layer",
-                        source: "hazard_hightide",
-                        type: "raster",
-                        paint: { "raster-opacity": 0.7 },
-                        layout: { visibility: "none" },
-                    },
-                    {
-                        id: "hazard_tsunami-layer",
-                        source: "hazard_tsunami",
-                        type: "raster",
-                        paint: { "raster-opacity": 0.7 },
-                        layout: { visibility: "none" },
-                    },
-                    {
-                        id: "hazard_doseki-layer",
-                        source: "hazard_doseki",
-                        type: "raster",
-                        paint: { "raster-opacity": 0.7 },
-                        layout: { visibility: "none" },
-                    },
-                    {
-                        id: "hazard_kyukeisha-layer",
-                        source: "hazard_kyukeisha",
-                        type: "raster",
-                        paint: { "raster-opacity": 0.7 },
-                        layout: { visibility: "none" },
-                    },
-                    {
-                        id: "hazard_jisuberi-layer",
-                        source: "hazard_jisuberi",
-                        type: "raster",
-                        paint: { "raster-opacity": 0.7 },
-                        layout: { visibility: "none" },
-                    },
                 ],
             },
         });
 
         mapInstance.on("load", () => {
-            const opacity = new OpacityControl({
-                baseLayers: {
-                    "empty-layer": "なし",
-                    "hazard_flood-layer": "洪水浸水想定区域",
-                    "hazard_hightide-layer": "高潮浸水想定区域",
-                    "hazard_tsunami-layer": "津波浸水想定区域",
-                    "hazard_doseki-layer": "土石流警戒区域",
-                    "hazard_kyukeisha-layer": "急傾斜警戒区域",
-                    "hazard_jisuberi-layer": "地滑り警戒区域",
-                },
-            });
-            mapInstance.addControl(opacity, "top-left");
-
             // // Add click event handler for the map
             // mapInstance.on("click", (e) => {
             //     const { lng, lat } = e.lngLat;
