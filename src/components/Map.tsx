f394f3add7e22b440402fed27e1d7a56c0b131a3
Masao Yano <masao.yano@gmail.com>
2025-04-05 15:45:23 +0000

    Update SQL query to source data from 'tokyo' table instead of 'uc14_ais_traffic'

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -79,7 +79,7 @@ const Map: React.FC<{ db: AsyncDuckDB }> = ({ db }) => {
                                 const query = `
                                     SELECT ST_AsGeoJSON(ST_Simplify(geom, 0.0000001)) AS geojson
                                     -- SELECT ST_AsGeoJSON(ST_Intersection(ST_Simplify(geom, 0.0001), ST_Simplify(ST_MakeEnvelope(${minLng}, ${minLat}, ${maxLng}, ${maxLat}), 0.0001))) AS geojson
-                                    FROM uc14_ais_traffic
+                                    FROM tokyo
                                     WHERE ST_Intersects(
                                         geom,
                                         ST_MakeEnvelope(${minLng}, ${minLat}, ${maxLng}, ${maxLat})
