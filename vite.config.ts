b996cd2dcddbe08c67aa3c518fc6d14600a4604a
Masao Yano <masao.yano@gmail.com>
2025-05-13 19:00:44 +0900

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/test-duckdb-react/',
     plugins: [react()],
     optimizeDeps: {
         exclude: ["@duckdb/duckdb-wasm"],
